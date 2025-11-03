4e4b0cab1a1977ef734d9b3209e711b3d90c558b
SnakeTnab <remy.simon.tnab@gmail.com>
2025-11-03 12:45:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def main():
 
     st.title("Amazon Client")
 
-    local_date = st.date_input("Date :", min_value=datetime(2022, 1, 1), max_value=datetime(2025, 1, 1))
+    local_date = st.date_input("Date :", min_value=datetime(2022, 1, 1), max_value=datetime(2200, 1, 1))
     route_number = st.text_input("Numéro de route :")
     scannable_id = st.text_input("Numéro de colis :")
 
